00c22e3854f48901ccc7ddb036bb76858cc29ac3
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2023-09-01 13:49:24 +0530

    new update

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,15 @@ import App from './App';
 test('renders learn react link', () => {
   const { container } = render(<App />);
   const linkElement = queryByText(container, /learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  // Check if the linkElement exists before asserting
+  if (linkElement) {
+    expect(linkElement).toBeInTheDocument();
+  } else {
+    // You can add a custom message or handle the case when the element is not found
+    // For example:
+    console.warn('The "learn react" link was not found in the rendered component.');
+    // Or you can fail the test explicitly
+    // fail('The "learn react" link was not found in the rendered component.');
+  }
 });
